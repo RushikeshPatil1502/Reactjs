4b290273e62bd96b1a2c91bcf04bed85f6f797f9
Rushikesh Patil <rushikesh1502@gmail.com>
2025-01-26 17:21:17 +0530

    initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className='text-black h-screen justify-center items-center flex flex-col bg-amber-100 w-full text font-bold'>
+      <div className="text-9xl">
+          hi 
+      </div>
+    </div>
+  )
+}
+
+export default App
